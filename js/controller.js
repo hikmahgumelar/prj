4878bee8fa1286b4ae0410e69baff053c9169806
Hikmah Gumelar <hikmahgumelar@yahoo.com>
2016-05-17 14:21:08 +0700

    merubah sedikit

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,9 @@
 var gapps = angular.module('gapps', []);
 gapps.controller("EventsController", function ($scope, $http) {
     $scope.proses = function() { 
-    var tujuan = "GMR" //$scope.tujuan;
-    var asal = "BD" //$scope.asal;
-    var newTanggal = "20160518"  //$scope.newTanggal;
+    var tujuan = $scope.tujuan;
+    var asal = $scope.asal;
+    var newTanggal = $scope.newTanggal;
 
     $http.get('/api/keretaapi?origination='+ asal + '&destination='+ tujuan + '&tanggal='+ newTanggal + '&apikey=TzW9lTM0RBsm2AgnL2M3AZgCZCcwz5hx_1412403086').
     success(function (data) {
